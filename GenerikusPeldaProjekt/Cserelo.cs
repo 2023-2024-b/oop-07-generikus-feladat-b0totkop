f749be04108bfb9db2d915984f783358ba95e1b2
kopy05 <tothkalocsaikoppany@gmail.com>
2024-09-20 11:16:28 +0200

    csere

--- GenerikusPeldaProjekt/Cserelo.cs
+++ GenerikusPeldaProjekt/Cserelo.cs
@@ -1,7 +1,13 @@
 ﻿namespace GenerikusPeldaProjekt
 {
-    public class Cserelo
+    public class Cserelo<T> where T : class
     {
         // Készítsen generikus osztályt amely tetszőleges két típust felcserél
+        public static void Csere(ref T elso, ref T masodik)
+        {
+            T temp = elso;
+            elso = masodik;
+            masodik = temp;
+        }
     }
 }
--- GenerikusPeldaProjekt/Program.cs
+++ GenerikusPeldaProjekt/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿using GenerikusPeldaProjekt;
+
+string jegy1 = "5";
+string jegy2 = "4";
+
+Cserelo<string>.Csere(ref jegy1, ref jegy2);
+
+Console.WriteLine(jegy1);
+Console.WriteLine(jegy2);
